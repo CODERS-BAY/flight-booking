cc0fac03a58c59bb5fb261861e860616850e5adc
Daniel Schwarzer <daniel.schwarzer@gmx.at>
2020-07-13 12:51:09 +0200

    Auto stash before merge of "dev" and "origin/dev"

--- src/main/java/dao/LuggageEntity.java
+++ src/main/java/dao/LuggageEntity.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package dao;
 
 import javax.persistence.*;
@@ -48,3 +49,55 @@ public class LuggageEntity {
         return result;
     }
 }
+=======
+package dao;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "luggage", schema = "flight_booking", catalog = "")
+public class LuggageEntity {
+    private int luggageId;
+    private int ticketId;
+
+    @Id
+    @Column(name = "luggage_id")
+    public int getLuggageId() {
+        return luggageId;
+    }
+
+    public void setLuggageId(int luggageId) {
+        this.luggageId = luggageId;
+    }
+
+    @Basic
+    @Column(name = "ticket_id")
+    public int getTicketId() {
+        return ticketId;
+    }
+
+    public void setTicketId(int ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        LuggageEntity that = (LuggageEntity) o;
+
+        if (luggageId != that.luggageId) return false;
+        if (ticketId != that.ticketId) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = luggageId;
+        result = 31 * result + ticketId;
+        return result;
+    }
+}
+>>>>>>> Stashed changes
--- src/main/java/dao/WebuserEntity.java
+++ src/main/java/dao/WebuserEntity.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package dao;
 
 import javax.persistence.*;
@@ -166,3 +167,173 @@ public class WebuserEntity {
         return result;
     }
 }
+=======
+package dao;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Table(name = "webuser", schema = "flight_booking", catalog = "")
+public class WebuserEntity {
+    private int webuserId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Date birthdate;
+    private int postCode;
+    private String city;
+    private String streetnumber;
+    private String username;
+    private String password;
+    private String phoneNumber;
+
+    @Id
+    @Column(name = "webuser_id")
+    public int getWebuserId() {
+        return webuserId;
+    }
+
+    public void setWebuserId(int webuserId) {
+        this.webuserId = webuserId;
+    }
+
+    @Basic
+    @Column(name = "first_name")
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Basic
+    @Column(name = "last_name")
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Basic
+    @Column(name = "email")
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Basic
+    @Column(name = "birthdate")
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    @Basic
+    @Column(name = "post_code")
+    public int getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(int postCode) {
+        this.postCode = postCode;
+    }
+
+    @Basic
+    @Column(name = "city")
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Basic
+    @Column(name = "streetnumber")
+    public String getStreetnumber() {
+        return streetnumber;
+    }
+
+    public void setStreetnumber(String streetnumber) {
+        this.streetnumber = streetnumber;
+    }
+
+    @Basic
+    @Column(name = "username")
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Basic
+    @Column(name = "password")
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Basic
+    @Column(name = "phone_number")
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        WebuserEntity that = (WebuserEntity) o;
+
+        if (webuserId != that.webuserId) return false;
+        if (postCode != that.postCode) return false;
+        if (firstName != null ? !firstName.equals(that.firstName) : that.firstName != null) return false;
+        if (lastName != null ? !lastName.equals(that.lastName) : that.lastName != null) return false;
+        if (email != null ? !email.equals(that.email) : that.email != null) return false;
+        if (birthdate != null ? !birthdate.equals(that.birthdate) : that.birthdate != null) return false;
+        if (city != null ? !city.equals(that.city) : that.city != null) return false;
+        if (streetnumber != null ? !streetnumber.equals(that.streetnumber) : that.streetnumber != null) return false;
+        if (username != null ? !username.equals(that.username) : that.username != null) return false;
+        if (password != null ? !password.equals(that.password) : that.password != null) return false;
+        if (phoneNumber != null ? !phoneNumber.equals(that.phoneNumber) : that.phoneNumber != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = webuserId;
+        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        result = 31 * result + (birthdate != null ? birthdate.hashCode() : 0);
+        result = 31 * result + postCode;
+        result = 31 * result + (city != null ? city.hashCode() : 0);
+        result = 31 * result + (streetnumber != null ? streetnumber.hashCode() : 0);
+        result = 31 * result + (username != null ? username.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (phoneNumber != null ? phoneNumber.hashCode() : 0);
+        return result;
+    }
+}
+>>>>>>> Stashed changes
