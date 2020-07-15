81e41f22be1d739fc54cbed0be80098085be01c0
Marco <myplace@posteo.net>
2020-07-15 09:05:30 +0200

    Update the Database files

--- src/main/java/service/AirportService.java
+++ src/main/java/service/AirportService.java
@@ -4,7 +4,6 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Path;
 
-import dao.AircraftEntity;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
