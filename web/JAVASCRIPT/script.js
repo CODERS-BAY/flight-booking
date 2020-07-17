189f951cf9b2bd0a425bc6dfab1830f02f187afe
Daniel Schwarzer <daniel.schwarzer@gmx.at>
2020-07-17 12:19:18 +0200

    Startsite / Login Reworked

--- web/JAVASCRIPT/script.js
+++ web/JAVASCRIPT/script.js
@@ -1,14 +1,19 @@
+
 $(document).ready(function () {
     console.log("js loaded");
 
 
     $(".loginButton").click(function () {
 
-        console.log("click");
-
         $(".popUp").css("display", "block");
         $(".greyBg").css("display", "none");
 
     });
 
-})
+    $(".fa-times").click(function () {
+
+        $(".popUp").css("display", "none");
+        $(".greyBg").css("display", "block");
+    });
+
+});
