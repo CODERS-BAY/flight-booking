ed2516c3223f328f2e9d116251d93efe2fd03812
Michael Wingelhofer <wingsch@gmx.at>
2020-07-17 10:58:57 +0200

    Update Database

--- src/main/java/dao/FlightEntity.java
+++ src/main/java/dao/FlightEntity.java
@@ -1,65 +1,54 @@
 package dao;
 
 import javax.persistence.*;
-import java.sql.Time;
+import java.sql.Timestamp;
 
 @Entity
 @Table(name = "flight", schema = "flight_booking", catalog = "")
 public class FlightEntity {
-    private String flightId;
-    private Time departureTime;
-    private Time arrivalTime;
-    private String arrivalIac;
-    private String departureIac;
+    private int flightId;
+    private Timestamp departureTime;
+    private Timestamp arrivalTime;
+    private double price;
 
     @Id
     @Column(name = "flight_id")
-    public String getFlightId() {
+    public int getFlightId() {
         return flightId;
     }
 
-    public void setFlightId(String flightId) {
+    public void setFlightId(int flightId) {
         this.flightId = flightId;
     }
 
     @Basic
     @Column(name = "departure_time")
-    public Time getDepartureTime() {
+    public Timestamp getDepartureTime() {
         return departureTime;
     }
 
-    public void setDepartureTime(Time departureTime) {
+    public void setDepartureTime(Timestamp departureTime) {
         this.departureTime = departureTime;
     }
 
     @Basic
     @Column(name = "arrival_time")
-    public Time getArrivalTime() {
+    public Timestamp getArrivalTime() {
         return arrivalTime;
     }
 
-    public void setArrivalTime(Time arrivalTime) {
+    public void setArrivalTime(Timestamp arrivalTime) {
         this.arrivalTime = arrivalTime;
     }
 
     @Basic
-    @Column(name = "arrival_IAC")
-    public String getArrivalIac() {
-        return arrivalIac;
+    @Column(name = "price")
+    public double getPrice() {
+        return price;
     }
 
-    public void setArrivalIac(String arrivalIac) {
-        this.arrivalIac = arrivalIac;
-    }
-
-    @Basic
-    @Column(name = "departure_IAC")
-    public String getDepartureIac() {
-        return departureIac;
-    }
-
-    public void setDepartureIac(String departureIac) {
-        this.departureIac = departureIac;
+    public void setPrice(double price) {
+        this.price = price;
     }
 
     @Override
@@ -69,23 +58,24 @@ public class FlightEntity {
 
         FlightEntity that = (FlightEntity) o;
 
-        if (flightId != null ? !flightId.equals(that.flightId) : that.flightId != null) return false;
+        if (flightId != that.flightId) return false;
+        if (Double.compare(that.price, price) != 0) return false;
         if (departureTime != null ? !departureTime.equals(that.departureTime) : that.departureTime != null)
             return false;
         if (arrivalTime != null ? !arrivalTime.equals(that.arrivalTime) : that.arrivalTime != null) return false;
-        if (arrivalIac != null ? !arrivalIac.equals(that.arrivalIac) : that.arrivalIac != null) return false;
-        if (departureIac != null ? !departureIac.equals(that.departureIac) : that.departureIac != null) return false;
 
         return true;
     }
 
     @Override
     public int hashCode() {
-        int result = flightId != null ? flightId.hashCode() : 0;
+        int result;
+        long temp;
+        result = flightId;
         result = 31 * result + (departureTime != null ? departureTime.hashCode() : 0);
         result = 31 * result + (arrivalTime != null ? arrivalTime.hashCode() : 0);
-        result = 31 * result + (arrivalIac != null ? arrivalIac.hashCode() : 0);
-        result = 31 * result + (departureIac != null ? departureIac.hashCode() : 0);
+        temp = Double.doubleToLongBits(price);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
         return result;
     }
 }
--- src/main/java/dao/TicketEntity.java
+++ src/main/java/dao/TicketEntity.java
@@ -1,14 +1,14 @@
 package dao;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "ticket", schema = "flight_booking", catalog = "")
 public class TicketEntity {
     private int ticketId;
+    private String seat;
+    private Byte business;
+    private int flightId;
 
     @Id
     @Column(name = "ticket_id")
@@ -20,6 +20,36 @@ public class TicketEntity {
         this.ticketId = ticketId;
     }
 
+    @Basic
+    @Column(name = "seat")
+    public String getSeat() {
+        return seat;
+    }
+
+    public void setSeat(String seat) {
+        this.seat = seat;
+    }
+
+    @Basic
+    @Column(name = "business")
+    public Byte getBusiness() {
+        return business;
+    }
+
+    public void setBusiness(Byte business) {
+        this.business = business;
+    }
+
+    @Basic
+    @Column(name = "flight_id")
+    public int getFlightId() {
+        return flightId;
+    }
+
+    public void setFlightId(int flightId) {
+        this.flightId = flightId;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -28,12 +58,19 @@ public class TicketEntity {
         TicketEntity that = (TicketEntity) o;
 
         if (ticketId != that.ticketId) return false;
+        if (flightId != that.flightId) return false;
+        if (seat != null ? !seat.equals(that.seat) : that.seat != null) return false;
+        if (business != null ? !business.equals(that.business) : that.business != null) return false;
 
         return true;
     }
 
     @Override
     public int hashCode() {
-        return ticketId;
+        int result = ticketId;
+        result = 31 * result + (seat != null ? seat.hashCode() : 0);
+        result = 31 * result + (business != null ? business.hashCode() : 0);
+        result = 31 * result + flightId;
+        return result;
     }
 }
