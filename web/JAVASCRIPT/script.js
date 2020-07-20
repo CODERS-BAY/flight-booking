44be503e670eba4e5758115dfed5f35042093ea6
Peter Baumgartner <baumgartner17@gmail.com>
2020-07-20 15:22:58 +0200

    DEPAirport KeyUP Function  added
    index + stylesheets updated

--- web/JAVASCRIPT/script.js
+++ web/JAVASCRIPT/script.js
@@ -1,13 +1,11 @@
-
 $(document).ready(function () {
-    console.log("js loaded");
+    //console.log("js loaded");
     get_json("hallo");
 
     $(".loginButton").click(function () {
 
         $(".popUp").css("display", "block");
         $(".greyBg").css("display", "none");
-
     });
 
     $(".fa-times").click(function () {
@@ -15,25 +13,53 @@ $(document).ready(function () {
         $(".popUp").css("display", "none");
         $(".greyBg").css("display", "block");
     });
-
 });
 
 function get_json(url) {
 
+    $.getJSON("http://lisacarina.at/bfi/flights.json", function (data) {
 
-    $.getJSON( "http://lisacarina.at/bfi/flights.json", function( data ) {
-
-        console.log(data[0]);
-        var items = [];
-        for (i = 0; i <= data.length; i++) {
-            $("#airportContainer").append("<li>" + data[i]["city"] + "</li>");
-            console.log(data[i]['city']);
+        for (let i = 0; i < data.length; i++) {
+            $("#depAirportList").append("<li data-city='" + data[i]['city'].toLowerCase() + "'data-iac='" + data[i]['IAC'].toLowerCase() + "' data-name='" + data[i]['name'].toLowerCase() + "' " +
+                "data-state='" + data[i]['state'].toLowerCase() + "' >" + data[i]['city'] + "<strong> (" + data[i]['IAC'] + ") " + "</strong></li>");
         }
     });
 
-}
+};
 
-$("#depAirport").keyup( function () {
+
+$("#depAirport").keyup(function () {
+    $("#depAirportList").css("display", "none");
+    $("#depAirportList li").css("display", "none");
+    let inputValue = $(this).val().toLowerCase();
+    $("#depAirportList li").each(function () {
+
+        if ($(this).data('city').indexOf(inputValue) > -1 || $(this).data('iac').indexOf(inputValue) > -1  || $(this).data('name').indexOf(inputValue) > -1 || $(this).data('state').indexOf(inputValue) > -1){
+            $(this).css("display", "block");
+        }else {
+            $("#depAirportList").css("display", "block")
+        }
+
+
+
+    });
 
 
 });
+
+
+function printallAirports() {
+    console.log("Test");
+    let container = "<div class='airport_container'>";
+    let i = 0;
+    for (data in employee) {
+        if (i == 0) {
+            container += "<p><strong>" + employee[data].toUpperCase() + "</strong></p>";
+        } else {
+            container += "<p><strong> " + data + ": </strong>" + employee[data] + "</p>";
+        }
+        i++;
+    }
+    container += "</div>";
+    $('#results').append(container);
+};
