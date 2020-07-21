8354a2a5a500fc2bd33ac20d8e03907a9ba33ea4
Daniel Schwarzer <daniel.schwarzer@gmx.at>
2020-07-21 12:18:47 +0200

    added functions: Login / Logout / setCookie / deleteCookie

--- web/JAVASCRIPT/script.js
+++ web/JAVASCRIPT/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(function (loginData) {
     //console.log("js loaded");
     get_json("hallo");
 
@@ -15,12 +15,14 @@ $(document).ready(function () {
     });
 });
 
+//-------------------- Flug Suche --------------------//
+
 function get_json(url) {
 
     $.getJSON("http://lisacarina.at/bfi/flights.json", function (data) {
 
         for (let i = 0; i < data.length; i++) {
-            console.log(data[i]);
+            //console.log(data[i]);
             $("#depAirportList").append("<li data-city='" + data[i]['city'].toLowerCase() + "'data-iac='" + data[i]['IAC'].toLowerCase() + "' data-name='" + data[i]['name'].toLowerCase() + "' " +
                 "data-state='" + data[i]['state'].toLowerCase() + "' >" + data[i]['name'] + "<strong> (" + data[i]['IAC'] + ") " + "</strong> " + data[i]['state'].toUpperCase() + " </li>");
         }
@@ -41,3 +43,66 @@ $("#depAirport").keyup(function () {
         }
     });
 });
+
+//-------------------- Login --------------------//
+
+$("#login").submit(function (e) {
+
+    e.preventDefault()
+    let userEmail = $("#email").val();
+
+    let loginData = {"email": userEmail, "password": $("#password").val()};
+
+    $.ajax ({
+        type: "post",
+        data: loginData,
+        url: "http://lisacarina.at/bfi/login.json",
+        success: function() {
+            setCookie("email", $("#email").val(), 20);
+            $(".loginButton").css("display", "none");
+            $(".logoutButton").css("display", "block");
+        }
+    })
+});
+
+//-------------------- Logout --------------------//
+
+$(".logoutButton").click( function () {
+    deleteCookie("email");
+    $(".loginButton").css("display", "block");
+    $(".logoutButton").css("display", "none");
+    $(".popUp").css("display", "none");
+    $(".greyBg").css("display", "block");
+});
+
+//------------------ Set Cookie -------------------//
+
+function setCookie(cname, cvalue, exdays) {
+    //console.log(cname);
+    //console.log(cvalue);
+    //console.log(exdays);
+    let d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires;
+    console.log("cookie saved");
+}
+
+//------------------ Delete Cookie -------------------//
+
+function deleteCookie(cname) {
+
+    document.cookie = cname + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
+    console.log("Cookie deleted");
+
+}
+
+
+
+
+
+
+
+
+
+
