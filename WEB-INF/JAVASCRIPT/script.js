1525bdbd8aa8eaf5d3aad094c6afcf8469bf960c
Peter Baumgartner <baumgartner17@gmail.com>
2020-07-23 10:32:05 +0200

    SearchButton added + script

--- WEB-INF/JAVASCRIPT/script.js
+++ WEB-INF/JAVASCRIPT/script.js
@@ -13,10 +13,8 @@ $(document).ready(function (loginData) {
     });
 });
 
-//-------------------- Flug Suche --------------------//
 
- // ------ DEPARTURE AIRPORT -----//
-
+//-------------------- JSON FOR FLIGHT SEARCH --------------------//
 function get_json(url) {
 
     $.getJSON("http://lisacarina.at/bfi/flights.json", function (data) {
@@ -30,11 +28,14 @@ function get_json(url) {
             $("#arrAirportList").append("<li data-id='" + i + "' data-city='" + data[i]['city'].toLowerCase() + "'data-iac='" + data[i]['IAC'].toLowerCase() + "' data-name='" + data[i]['name'].toLowerCase() + "' " +
                 "data-state='" + data[i]['state'].toLowerCase() + "' class='text' >" + data[i]['name'] + "<strong> (" + data[i]['IAC'] + ") " + "</strong> " + data[i]['state'].toUpperCase() + " </li>");
 
+            let iac = data[i]['IAC'];
+            console.log(iac);
         }
     });
 
 };
 
+//-------------------- DEPARTURE AIRPORT --------------------//
 $("#depAirport").keyup(function () {
     $("#depAirportList").css("display", "none");
     $("#depAirportList li").css("display", "none");
@@ -45,38 +46,34 @@ $("#depAirport").keyup(function () {
             $(this).css("display", "block");
         } else {
             $("#depAirportList").css("display", "block")
-
         }
+        checkInputValue()
     });
 });
 
-
-$("#depAirportList").on('click','li',function (){
+$("#depAirportList").on('click', 'li', function () {
     let depIAC = $(this).attr('data-iac');
     let depAirport = $(this).text();
-    console.log(depIAC);
-    console.log(depAirport);
 
     $('#depAirport').val(depAirport);
+
+    $('#depAirport').attr({'data-iac': depIAC});
+    //$('#depAirport').('iac', depIAC);
     $("#depAirportList").css("display", "none");
 
-
     let data = {depIAC};
-    $.ajax ({
+    $.ajax({
         type: "post",
         data: data,
         url: "http://lisacarina.at/bfi/login.json",
-        success: function() {
+        success: function () {
             console.log('TEST')
         }
     })
 
 });
 
-
-// ------ ARRIVAL AIRPORT -----//
-
-
+//-------------------- ARRIVAL AIRPORT --------------------//
 $("#arrAirport").keyup(function () {
     $("#arrAirportList").css("display", "none");
     $("#arrAirportList li").css("display", "none");
@@ -88,35 +85,33 @@ $("#arrAirport").keyup(function () {
         } else {
             $("#arrAirportList").css("display", "block")
         }
+        checkInputValue()
     });
 });
 
-$("#arrAirportList").on('click','li',function (){
-    let depID = $(this).attr('data-iac');
+$("#arrAirportList").on('click', 'li', function () {
+    let arrID = $(this).attr('data-iac');
     let arrAirport = $(this).text();
-    console.log(depID);
-    console.log(arrAirport);
 
     $('#arrAirport').val(arrAirport);
+    $('#arrAirport').attr({'data-iac': arrID});
     $("#arrAirportList").css("display", "none");
+
 
 });
 
-
 //-------------------- Login --------------------//
-
 $("#login").submit(function (e) {
 
     e.preventDefault()
     let userEmail = $("#email").val();
-
     let loginData = {"email": userEmail, "password": $("#password").val()};
 
-    $.ajax ({
+    $.ajax({
         type: "post",
         data: loginData,
         url: "http://lisacarina.at/bfi/login.json",
-        success: function() {
+        success: function () {
             setCookie("email", $("#email").val(), 20);
             $(".loginButton").css("display", "none");
             $(".logoutButton").css("display", "block");
@@ -125,8 +120,8 @@ $("#login").submit(function (e) {
 });
 
 //-------------------- Logout --------------------//
+$(".logoutButton").click(function () {
 
-$(".logoutButton").click( function () {
     deleteCookie("email");
     $(".loginButton").css("display", "block");
     $(".logoutButton").css("display", "none");
@@ -134,29 +129,55 @@ $(".logoutButton").click( function () {
     $(".greyBg").css("display", "block");
 });
 
-//------------------ Set Cookie -------------------//
+//-------------------- SEARCHBUTTON ONCLICK --------------------//
+$('#searchButton').on('click', function (event) {
 
+    urlCreator();
+});
+
+
+//-------------------- FUNCTIONS --------------------//
+
+//------------------ Set Cookie -------------------//
 function setCookie(cname, cvalue, exdays) {
     //console.log(cname);
     //console.log(cvalue);
     //console.log(exdays);
     let d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    let expires = "expires="+ d.toUTCString();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires;
     console.log("cookie saved");
 }
 
 //------------------ Delete Cookie -------------------//
-
 function deleteCookie(cname) {
 
     document.cookie = cname + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
-    console.log("Cookie deleted");
-
+    //console.log("Cookie deleted");
 }
 
 
+//------------------ CREATE URL -------------------//
+function urlCreator() {
+    $('#searchButton').attr('href', function (index, href) {
+        const url = new URL(window.location);
+        console.log(url);
+        url.searchParams.append('dep', $('#depAirport').attr('data-iac'));
+        url.searchParams.append('arr', $('#arrAirport').attr('data-iac'));
+        console.log(url);
+    });
+}
+
+//------------------ CHECK INPUTVALUE FOR SEARCHBUTTON -------------------//
+function checkInputValue() {
+
+    if ($('#depAirport').val().length != 0 && $('#arrAirport').val().length != 0) {
+        $('#searchButton').attr("disabled", false);
+    } else {
+        $('#searchButton').attr("disabled", true);
+    }
+}
 
 
 
