f1008ea28eb99281d41f4a8b5e0f89c545584147
Martin <martin.volb@gmx.at>
2020-07-23 13:11:40 +0200

    method for flightselect servlett

--- src/main/java/service/GetSelectedFlight.java
+++ src/main/java/service/GetSelectedFlight.java
@@ -1,12 +1,41 @@
 package service;
 
 import com.google.gson.Gson;
+import dao.FlightEntity;
+import model.HibernatePersister;
+import org.hibernate.Session;
 
+import javax.persistence.Query;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
 
-public class GetSelectedFlight {
-    Gson gson = new Gson();
 
-    @POST
+    @Path("/getSelectedFlight")
+    public class GetSelectedFlight {
+        Gson gson = new Gson();
+
+        @POST
+        @Consumes(MediaType.APPLICATION_JSON)
+        @Produces(MediaType.APPLICATION_JSON)
+        public String selectedFlight(String userJson) {
+            FlightEntity flightEntity = gson.fromJson(userJson, FlightEntity.class);
+            HibernatePersister hibernatePersister = new HibernatePersister();
+
+            Session session = hibernatePersister.getSessionFactory().openSession();
+            session.beginTransaction();
+            String hql = "from FlightEntity F where F.departureIac= :depature_IAC and F.arrivalIac= :arrival_IAC and F.departureTime = :depature_time";
+            Query query = session.createQuery(hql);
+            query.setParameter("depature_IAC", flightEntity.getDepartureIac()).setParameter( "arrival_IAC", flightEntity.getArrivalIac()).setParameter("depature_time", flightEntity.getDepartureTime());
+            List flightList = query.getResultList();
+
+
+            String selectedFlight = gson.toJson(flightList);
+
+            return selectedFlight;
+        }
     
 }
