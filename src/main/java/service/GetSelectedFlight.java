62270ee57db726a07f19b35e60f804b5db0f278d
Martin <martin.volb@gmx.at>
2020-07-24 11:31:42 +0200

    commit for saving version

--- src/main/java/service/GetSelectedFlight.java
+++ src/main/java/service/GetSelectedFlight.java
@@ -1,7 +1,9 @@
 package service;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 import dao.FlightEntity;
+import dao.TicketEntity;
 import model.HibernatePersister;
 import org.hibernate.Session;
 
@@ -18,39 +20,80 @@ import java.util.Date;
 import java.util.List;
 
 
-    @Path("/getSelectedFlight")
-    public class GetSelectedFlight {
-        Gson gson = new Gson();
+@Path("/getSelectedFlight")
+public class GetSelectedFlight {
+    Gson gson = new Gson();
 
-        @POST
-        @Consumes(MediaType.APPLICATION_JSON)
-        @Produces(MediaType.APPLICATION_JSON)
-        public String selectedFlight(String userJson) {
-            FlightEntity flights = gson.fromJson(userJson, FlightEntity.class);
-            HibernatePersister hibernatePersister = new HibernatePersister();
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String selectedFlight(String userJson) {
+        FlightEntity flights = gson.fromJson(userJson, FlightEntity.class);
+        HibernatePersister hibernatePersister = new HibernatePersister();
 
-            Timestamp date = flights.getDepartureTime();
+        Timestamp date = flights.getDepartureTime();
 
-            Calendar cal = Calendar.getInstance();
-            cal.setTime(date);
-            cal.add(Calendar.DAY_OF_WEEK, 4);
-            Timestamp dateEnd = new Timestamp(cal.getTime().getTime());
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.add(Calendar.DAY_OF_WEEK, 4);
+        Timestamp dateEnd = new Timestamp(cal.getTime().getTime());
 
-            cal.add(Calendar.DAY_OF_WEEK,-7);
-            Timestamp dateStart = new Timestamp(cal.getTime().getTime());
-
-
-            Session session = hibernatePersister.getSessionFactory().openSession();
-            session.beginTransaction();
-            String hql = "from FlightEntity F where F.departureIac= :departure_IAC and F.arrivalIac= :arrival_IAC and F.departureTime >= :dateStart and F.departureTime <= :dateEnd";
-            Query query = session.createQuery(hql);
-            query.setParameter("departure_IAC", flights.getDepartureIac()).setParameter("arrival_IAC", flights.getArrivalIac()).setParameter("dateStart", dateStart).setParameter("dateEnd", dateEnd);
-            List flightList = query.getResultList();
+        cal.add(Calendar.DAY_OF_WEEK, -7);
+        Timestamp dateStart = new Timestamp(cal.getTime().getTime());
 
 
-            String selectedFlight = gson.toJson(flightList);
+        Session session = hibernatePersister.getSessionFactory().openSession();
+        session.beginTransaction();
+        String hql = "from FlightEntity F where F.departureIac= :departure_IAC and F.arrivalIac= :arrival_IAC and F.departureTime >= :dateStart and F.departureTime <= :dateEnd";
+        Query query = session.createQuery(hql);
+        query.setParameter("departure_IAC", flights.getDepartureIac()).setParameter("arrival_IAC", flights.getArrivalIac()).setParameter("dateStart", dateStart).setParameter("dateEnd", dateEnd);
+        List<FlightEntity> flightList = query.getResultList();
 
-            return selectedFlight;
+        for (FlightEntity flight : flightList) {
+
+           getAvailableSeats(flight);
+
+
         }
-    
+
+
+        String selectedFlight = gson.toJson(flightList);
+
+        return selectedFlight;
+    }
+
+    /**
+     * Counts available Seats for specific Flight and returns arr of business and economy seats
+     * arr[0] business seats
+     * arr[1] economy seats
+     */
+    private FlightEntity getAvailableSeats(FlightEntity flight) {
+        final int totalBusinessSeats = 50; // add real seat values
+        final int totalEconomySeats = 100; // add real seat values
+
+        HibernatePersister hibernatePersister = new HibernatePersister();
+        int[] availableSeats = new int[] {totalBusinessSeats,totalEconomySeats};
+
+        Session session = hibernatePersister.getSessionFactory().openSession();
+        session.beginTransaction();
+        String hql = "FROM TicketEntity T where T.flightId = :flightId";
+        Query query = session.createQuery(hql).setParameter("flightId", flight.getFlightId());
+        List<TicketEntity> ticketList = query.getResultList();
+
+        for (TicketEntity ticket: ticketList) {
+            if (ticket.getBusiness() == 1) {
+                availableSeats[0]--;
+            } else  {
+                availableSeats[1]--;
+            }
+        }
+
+        JsonElement jsonElement = gson.toJsonTree(flight);
+        jsonElement.getAsJsonObject().addProperty("businessSeat", availableSeats[0]);
+        jsonElement.getAsJsonObject().addProperty("economySeat", availableSeats[1]);
+
+        return gson.fromJson(jsonElement, FlightEntity.class);
+
+    }
+
 }
