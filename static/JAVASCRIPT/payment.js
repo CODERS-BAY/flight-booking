0df07e7d61f40ed8485819d7a5b5ac7236d75fd0
Daniel Schwarzer <daniel.schwarzer@gmx.at>
2020-08-03 15:19:33 +0200

    Payment Styling

--- static/JAVASCRIPT/payment.js
+++ static/JAVASCRIPT/payment.js
@@ -14,18 +14,30 @@ function generatePassengerForm(persons) {
     let i = 0;
 
 
+    $('#paymentData').append("<div class='col-12 passengerRow'>" +
+        "<form class='passengerForm'>" +
+        "<input id='cardNumber' class='paymentInput' type='text' name='cardNumber' placeholder='Kartennummer' required>" +
+        "<input class='paymentInput' type='text' name='validityDate' placeholder='G&uuml;ltigkeitsdatum' required>" +
+        "<input class='paymentInput' type='text' name='verififactionNumber' placeholder='CVC' required>" +
+        "<input class='paymentInput' type='text' name='cardOwner' placeholder='Karteninhaber' required>" +
+        "<select class='paymentInput' for='cardType' name='cardType' ><option value='mastercard'>Mastercard</option><option value='visa'>Visa</option></select> " +
+        "</form>" +
+        "</div>");
+
+
+
     for (i; i < persons; i++) {
 
         $('#passengerData').append("<div class='col-12 passengerRow'>" +
             "<form class='passengerForm'>" +
-                    "<input type='text' name='fistName' placeholder='Vorname' required>" +
-                    "<input type='text' name='lastName' placeholder='Nachname' required>" +
-                    "<input type='text' name='street' placeholder='Stra&szlig;e + Hausnummer'>" +
-                    "<input type='number' name='zip' placeholder='PLZ'>" +
-                    "<input type='text' name='city' placeholder='Stadt'>" +
-                    "<input type='text' name='state' placeholder='Land'>" +
-                    "<input type='email' name='email' placeholder='E-Mail Adresse' required>" +
-                    "<input type='number' name='phonenumber' placeholder='Telefonnummer'>" +
+                    "<input class='paymentInput' type='text' name='fistName' placeholder='Vorname' required>" +
+                    "<input class='paymentInput' type='text' name='lastName' placeholder='Nachname' required>" +
+                    "<input class='paymentInput' type='text' name='street' placeholder='Stra&szlig;e + Hausnummer'>" +
+                    "<input class='paymentInput' type='number' name='zip' placeholder='PLZ'>" +
+                    "<input class='paymentInput' type='text' name='city' placeholder='Stadt'>" +
+                    "<input class='paymentInput' type='text' name='state' placeholder='Land'>" +
+                    "<input class='paymentInput' type='email' name='email' placeholder='E-Mail Adresse' required>" +
+                    "<input class='paymentInput' type='number' name='phonenumber' placeholder='Telefonnummer'>" +
                 "</form>" +
             "</div>");
     }
@@ -33,3 +45,8 @@ function generatePassengerForm(persons) {
     $(".passengerRow").css("flex-direction", "column");
 
 }
+
+$("#cardNumber").keyup( function () {
+   let cardNumber = $("#cardNumber").val();
+   
+});
